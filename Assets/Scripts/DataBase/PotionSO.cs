c106819125757a2c481086896a592b0eaec082f0
kicadrica <92972020+kicadrica@users.noreply.github.com>
2022-09-10 21:24:42 +0200

    Initial commit

--- Assets/Scripts/DataBase/PotionSO.cs
+++ Assets/Scripts/DataBase/PotionSO.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "Potion", menuName = "Items/Potion")]
 public class PotionSO : ItemSO
